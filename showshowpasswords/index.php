42f0073be6837c4229e186ce2a60cf9fbbd5ebae
stenehall <stenehall@gmail.com>
2010-09-06 13:27:06 +0200

    Adding showpasswords

---
+++ showshowpasswords/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en-us" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title></title>
+    	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.showpasswords.js"></script>
+		<script type="text/javascript">
+		  jQuery(document).ready(function() {
+			jQuery.showpasswords();
+		  });
+		</script>
+	</head>
+<body>
+<form action="index_submit" method="get" accept-charset="utf-8">
+	<p><input type="password" name="some_name" value="lösenord" id="some_name"></p>
+
+</form>
+</body>
+</html>
